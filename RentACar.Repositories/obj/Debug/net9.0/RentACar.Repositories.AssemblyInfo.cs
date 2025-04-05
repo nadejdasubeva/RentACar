3c3480294f5ccdb291d539b045e4e2940454400b
Nadejda Subeva <123803434+nadejdasubeva@users.noreply.github.com>
2025-04-06 00:58:33 +0300

    Im kms video click here not clickbait

--- RentACar.Repositories/obj/Debug/net9.0/RentACar.Repositories.AssemblyInfo.cs
+++ RentACar.Repositories/obj/Debug/net9.0/RentACar.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f5b391034da18317279d81ed6753e9e20ca6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be6377b4244752b61c29db211a7ee42240b21be")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
