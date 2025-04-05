fb878a10afc451660d9539850bcfbc6edea5861b
Nadejda Subeva <123803434+nadejdasubeva@users.noreply.github.com>
2025-04-05 23:35:31 +0300

    user controller done

--- Models/obj/Debug/net9.0/RentACar.Data.AssemblyInfo.cs
+++ Models/obj/Debug/net9.0/RentACar.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27accc9108458a22fd69ebc6924aef88599b3369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfadc42df8fd53e496302ebe5a07ad5033882ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RentACar/obj/Debug/net9.0/RentACar.Web.AssemblyInfo.cs
+++ RentACar/obj/Debug/net9.0/RentACar.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27accc9108458a22fd69ebc6924aef88599b3369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfadc42df8fd53e496302ebe5a07ad5033882ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
